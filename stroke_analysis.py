5a18e6e55028e19fa425f523826fca5b69540977
sreeshareddy12 <sreeshareddy.12@gmail.com>
2023-11-30 00:25:51 -0800

    commit

--- stroke_analysis.py
+++ stroke_analysis.py
@@ -6,6 +6,46 @@ from sklearn.metrics import confusion_matrix
 # Read Csv data file
 df = pd.read_csv('/Users/sreeshareddy/Desktop/healthcare-dataset-stroke-data.csv')
 
+# display number of rows and columns
+print(df.shape)
+
+# copy and get the statistics for numerical columns
+summary = df.describe()
+summary.to_clipboard()
+
+# copy and get first 5 rows
+summary = df.head()
+summary.to_clipboard()
+
+# Calculate skewness for numerical columns
+Age = df['age']
+Bmi = df['bmi']
+Avg_glucose = df['avg_glucose_level']
+# Print skewness values
+print("Age Skewness:", sps.skew(Age))
+print("Bmi Skewness:", sps.skew(Bmi))
+print("Avg_glucose:", sps.skew(Avg_glucose))
+
+# Check for NULL values
+print(df.isnull().sum())
+
+# count for values in categorical columns
+sns.countplot(x='work_type', data=df)
+plt.show()
+# count for values in categorical columns
+sns.countplot(x='Residence_type', data=df)
+plt.show()
+# count for values in categorical columns
+sns.countplot(x='ever_married', data=df)
+plt.show()
+# count for values in categorical columns
+sns.countplot(x='smoking_status', data=df)
+plt.show()
+# count for values in categorical columns
+sns.countplot(x='gender', data=df)
+plt.show()
+
+
 
 # Drop 'Other' from 'gender' column
 df.drop(df[df['gender'] == 'Other'].index, axis=0, inplace=True)
@@ -33,6 +73,31 @@ df.drop(['gender','ever_married','work_type', 'Residence_type'], axis=1, inplace
 df1 = df.dropna(subset=['bmi'])
 df1.to_csv('df1.csv', index=False)
 
+# Check for outliers
+# Generate boxplot and histplot for  bmi
+sns.boxplot(x='bmi', data=df1, color='b')
+plt.title("Boxplot of 'bmi'")
+plt.show()
+sns.histplot(df1, x='bmi', kde=True, color='b')
+plt.title("Histplot of 'bmi'")
+plt.show()
+# Generate boxplot and histplot for age
+sns.boxplot(x='age', data=df1, color='y')
+plt.title("Boxplot of 'age'")
+plt.show()
+sns.histplot(df1, x='age', kde=True, color='y')
+plt.title("Histplot of 'age'")
+plt.show()
+# Generate boxplot and histplot for average glucose level
+sns.boxplot(x='avg_glucose_level', data=df1, color='r')
+plt.title("Boxplot of 'avg_glucose_level'")
+plt.show()
+sns.histplot(df1, x='avg_glucose_level', kde=True, color='r')
+plt.title("Histplot of 'avg_glucose_level'")
+plt.show()
+
+
+
 
 column_names = ['bmi', 'avg_glucose_level']
 
@@ -47,6 +112,18 @@ outliers = df1['bmi'][(df1['bmi'] < q1 - 1.5 * iqr) | (df1['bmi'] > q3 + 1.5 * i
 
 # Remove outliers from both columns
 df1 = df1[df1['bmi'].isin(outliers) == False]
+
+# Generate boxplot and histplot after removing outliers
+sns.boxplot(x='bmi', data=df1, color='c')
+plt.show()
+sns.histplot(df1, x='bmi', kde=True, color='c')
+plt.show()
+
+# Generate correlation matrix or heatmap
+sns.heatmap(df1.corr(), annot=True)
+plt.title("Heatmap of stroke Correlation")
+plt.show()
+
 
 # Select predictor (X) and target (y) variables
 X_age = df1[["age"]]  # Predictor variable: age
@@ -216,17 +293,6 @@ sns.set(color_codes=True)
 sns.regplot(x="bmi", y="stroke", data=df1, logistic=True, scatter_kws={"alpha": 0.3, "color":'b'}, color='orange', label='BMI')
 plt.title("BMI vs. Stroke")
 plt.legend(loc='upper right')
-
-
-# Married vs. Stroke
-plt.subplot(2, 3, 6)
-sns.set(color_codes=True)
-sns.regplot(x="Married", y="stroke", data=df1, logistic=True, scatter_kws={"alpha": 0.3, "color":'b'}, color='orange', label='Ever_married')
-plt.title("Ever_married vs. Stroke")
-plt.legend(loc='upper right')
-
-
-
 
 
 
